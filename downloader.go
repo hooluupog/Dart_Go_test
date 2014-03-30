23cd1e60baf6724a360d221d0c4dc309b7884d34
hooluupog <qq510371827@gmail.com>
2014-03-30 19:30:14 +0800

    add resume downloding feature

--- downloader.go
+++ downloader.go
@@ -6,19 +6,85 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
 func do(err error, info ...interface{}) {
 	if err != nil {
-		log.Fatal(info, "\n", err)
+		log.Fatal("\n", info, "\n", err)
 	}
 }
 
-func Copy(dst io.Writer, src io.Reader, data chan int64) (written int64, err error) {
+func isConnected(url string) bool {
+	_, err := http.Head(url)
+	if err != nil {
+		return false
+	}
+	return true
+}
+func new_download(url string) io.Reader {
+	client := &http.Client{}
+	for i := 0; i < 10; i++ {
+		time.Sleep(10000 * time.Millisecond)
+		fmt.Println("Trying ", i+1, " times")
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			continue
+		}
+		response, err := client.Do(req)
+		if err != nil {
+			continue
+		}
+		fmt.Println("Connected.Restart downloading.")
+		return response.Body
+	}
+	return nil
+}
+func resume_download(url string, fileSize, fileLength int64) io.Reader {
+	client := &http.Client{}
+	for i := 0; i < 10; i++ {
+		time.Sleep(10000 * time.Millisecond)
+		fmt.Println("Trying ", i+1, " times")
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			continue
+		}
+		contentRange := "bytes=" + strconv.FormatInt(fileSize, 10) + "-" + strconv.FormatInt(fileLength-1, 10)
+		req.Header.Add("Range", contentRange)
+		response, err := client.Do(req)
+		if err != nil {
+			continue
+		}
+		fmt.Println("Connected.Resume downloading.")
+		return response.Body
+	}
+	return nil
+}
+func transfer(dst io.Writer, src io.Reader, url string, fileName string, fileLength int64, data chan int64) (written int64, err error) {
 	buf := make([]byte, 32*1024)
 	for {
+		if !isConnected(url) {
+			fmt.Println("\nConnection closed.Retry connecting...")
+			stat, err := os.Stat(fileName)
+			if err != nil {
+				output, err := os.Create(fileName)
+				do(err, "Can not create ", fileName)
+				fmt.Println("File broken.Will restart downloaing.")
+				defer output.Close()
+				dst = output
+				src = new_download(url)
+				written = 0
+			} else {
+				fileSize := stat.Size()
+				src = resume_download(url, fileSize, fileLength)
+			}
+			if src == nil {
+				fmt.Println("download failed.")
+				os.Exit(1)
+			}
+		}
 		nr, er := src.Read(buf)
 		if nr > 0 {
 			nw, ew := dst.Write(buf[0:nr])
@@ -37,6 +103,7 @@ func Copy(dst io.Writer, src io.Reader, data chan int64) (written int64, err err
 		}
 		if er == io.EOF {
 			data <- -1
+			break
 		}
 		if er != nil {
 			err = er
@@ -51,14 +118,18 @@ func downloadFromUrl(url string, data chan int64) {
 	output, err := os.Create(fileName)
 	do(err, "Error while creating ", fileName)
 	defer output.Close()
-	response, err := http.Get(url)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	do(err, "Error while requesting ", url)
+	response, err := client.Do(req)
 	do(err, "Error while downloading ", url)
 	if response.StatusCode == 404 {
 		log.Fatal("file NotFound: ", url)
 	}
 	defer response.Body.Close()
-	data <- response.ContentLength
-	_, err = Copy(output, response.Body, data)
+	fileLength := response.ContentLength
+	data <- fileLength
+	_, err = transfer(output, response.Body, url, fileName, fileLength, data)
 	do(err, "Error while downloading ", url)
 }
 
@@ -83,6 +154,9 @@ func progress(data chan int64) {
 		i := int(present * 50)
 		h := strings.Repeat("=", i) + strings.Repeat(" ", 50-i)
 		duration := time.Since(start).Seconds()
+		if currentSize < lastSize {
+			lastSize = currentSize
+		}
 		size = size + currentSize - lastSize
 		speed = updateSpeed
 		lastSize = currentSize
@@ -98,7 +172,7 @@ func progress(data chan int64) {
 func main() {
 	data := make(chan int64)
 	//url := "http://www.baidu.com/img/bdlogo.gif"
-	url := "http://releases.ubuntu.com/trusty/ubuntu-14.04-beta2-desktop-i386.iso"
+	url := "http://releases.ubuntu.com/14.04/ubuntu-14.04-beta2-desktop-i386.iso"
 	go downloadFromUrl(url, data)
 	progress(data)
 	fmt.Print("\nDownload finished.")
